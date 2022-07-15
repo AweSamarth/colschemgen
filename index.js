a356407bf4fc540307f727a78766b34b8bc5543a
Samarth Saxena <samarthsaxena1672003@gmail.com>
2022-07-15 14:48:56 +0530

    'huhhhhhhhhhhhhhh

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
-console.log("hi lol")
 let hexval="0047AB"
-fetch("https://www.thecolorapi.com/scheme?hex=${hexval}&mode=analogic")
-    .then(response=>response.json())
-    .then(data=>console.log(data))
+// fetch("https://www.thecolorapi.com/scheme?hex=${hexval}&mode=analogic")
+//     .then(response=>response.json())
+//     .then(data=>console.log(data))
+
+const colour= document.getElementById("seed-color").value
+console.log(colour)
