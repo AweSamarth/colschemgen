24ebd361c1038a6d0da1d2688abe4858551b5bf2
Samarth Saxena <samarthsaxena1672003@gmail.com>
2022-07-15 20:04:39 +0530

    kaam chalau

--- index.js
+++ index.js
@@ -4,15 +4,24 @@ let hexval="0047AB"
 //     .then(response=>response.json())
 //     .then(data=>console.log(data))
 
-const clicked=document.getElementById("crow")
+checker()
+    
 
 crow.addEventListener("click", checker)
 function checker(){
+    
     const colour= document.getElementById("seed-color").value.substring(1)
+    const mode= document.getElementById("color-scheme").value.toLowerCase()
     console.log(colour)
-    fetch("https://www.thecolorapi.com/scheme?hex=${colour}&mode=analogic")
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colour}&mode=${mode}`)
     .then(response=>response.json())
     .then(data=>{
-        for(let i=0;i<5;i++){
-        console.log(data.colors[i].hex.value)}})
+        for (let i=0;i<5;i++){
+        document.getElementById(`c${i}`).style.background=data.colors[i].hex.value
+    }
+    }
+    
+
+    )
+    
 }
