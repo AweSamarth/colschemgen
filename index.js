8925b3da57bcf971381ac91b54d46ff4330bb7e5
Samarth Saxena <samarthsaxena1672003@gmail.com>
2022-07-15 18:50:19 +0530

    moment moment

--- index.js
+++ index.js
@@ -4,5 +4,15 @@ let hexval="0047AB"
 //     .then(response=>response.json())
 //     .then(data=>console.log(data))
 
-const colour= document.getElementById("seed-color").value
-console.log(colour)
+const clicked=document.getElementById("crow")
+
+crow.addEventListener("click", checker)
+function checker(){
+    const colour= document.getElementById("seed-color").value.substring(1)
+    console.log(colour)
+    fetch("https://www.thecolorapi.com/scheme?hex=${colour}&mode=analogic")
+    .then(response=>response.json())
+    .then(data=>{
+        for(let i=0;i<5;i++){
+        console.log(data.colors[i].hex.value)}})
+}
